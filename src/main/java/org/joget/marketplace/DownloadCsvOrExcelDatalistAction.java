306de357f5647a37987e30895e2877dd39a1976a
muhammad.imran <muhammad.imran@joget.com>
2023-08-11 02:26:43 +0800

    Fixed the CSV download
    Download will start immediately, it won't wait the whole file to be generated and then start download. more like asynchronous file download.

--- src/main/java/org/joget/marketplace/DownloadCsvOrExcelDatalistAction.java
+++ src/main/java/org/joget/marketplace/DownloadCsvOrExcelDatalistAction.java
@@ -25,6 +25,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Collection;
@@ -32,7 +33,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.commons.lang3.math.NumberUtils;
-
 
 public class DownloadCsvOrExcelDatalistAction extends DataListActionDefault {
 
@@ -43,8 +43,9 @@ public class DownloadCsvOrExcelDatalistAction extends DataListActionDefault {
     public String getName() {
         return "Download CSV or Excel Datalist Action";
     }
+
     public String getVersion() {
-        return "8.0.0";
+        return "8.0.1";
     }
 
     public String getClassName() {
@@ -99,17 +100,17 @@ public class DownloadCsvOrExcelDatalistAction extends DataListActionDefault {
         return downloadAs.equalsIgnoreCase("csv");
     }
 
-    public boolean getFooter(){
+    public boolean getFooter() {
         String footer = getPropertyString("footerHeader");
         return footer.equalsIgnoreCase("true");
     }
 
-    public boolean includeCustomHeader(){
+    public boolean includeCustomHeader() {
         String header = getPropertyString("includeCustomHeader");
         return header.equalsIgnoreCase("true");
     }
 
-    public boolean includeCustomFooter(){
+    public boolean includeCustomFooter() {
         String footer = getPropertyString("includeCustomFooter");
         return footer.equalsIgnoreCase("true");
     }
@@ -123,11 +124,11 @@ public class DownloadCsvOrExcelDatalistAction extends DataListActionDefault {
         }
 
         // check for submited rows
-        if ( (rowKeys != null && rowKeys.length > 0) || getProperty("downloadAllWhenNoneSelected").equals("true") ) {
+        if ((rowKeys != null && rowKeys.length > 0) || getProperty("downloadAllWhenNoneSelected").equals("true")) {
             try {
                 //get the HTTP Response
                 HttpServletResponse response = WorkflowUtil.getHttpServletResponse();
-                if(getDownloadAs()) {
+                if (getDownloadAs()) {
                     downloadCSV(request, response, dataList, rowKeys);
                 } else {
                     downloadExcel(request, response, dataList, rowKeys);
@@ -138,25 +139,27 @@ public class DownloadCsvOrExcelDatalistAction extends DataListActionDefault {
                 throw new RuntimeException(e);
             }
         }
-
-        //return null to do nothing
         return null;
     }
 
     protected void downloadCSV(HttpServletRequest request, HttpServletResponse response, DataList dataList, String[] rowKeys) throws ServletException, IOException {
-        byte[] bytes = getCSV(dataList, rowKeys);
-        String filename = getPropertyString("renameFile").equalsIgnoreCase("true") ? getPropertyString("filename") + ".csv" :"report.csv";
-        writeResponse(request, response, bytes, filename, "text/csv");
+
+        String filename = getPropertyString("renameFile").equalsIgnoreCase("true") ? getPropertyString("filename") + ".csv" : "report.csv";
+
+        response.setContentType("text/csv");
+        response.setHeader("Content-Disposition", "attachment; filename=" + filename + "");
+
+        try ( OutputStream outputStream = response.getOutputStream()) {
+            PrintWriter writer = new PrintWriter(outputStream);
+            streamCSV(request, response, writer, dataList, rowKeys);
+            writer.flush();  // Flush any remaining buffered data
+            outputStream.flush();  // Flush the output stream
+            writer.close();
+        }
     }
 
-    protected void downloadExcel(HttpServletRequest request, HttpServletResponse response, DataList dataList, String[] rowKeys) throws ServletException, IOException {
-        Workbook workbook = getExcel(dataList, rowKeys);
-        String filename = getPropertyString("renameFile").equalsIgnoreCase("true") ? getPropertyString("filename") + ".xlsx" :"report.xlsx";
-        writeResponseExcel(request, response, workbook, filename, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet\n");
-    }
+    protected void streamCSV(HttpServletRequest request, HttpServletResponse response, PrintWriter writer, DataList dataList, String[] rowKeys) throws IOException {
 
-    protected byte[] getCSV(DataList dataList, String[] rowKeys) throws IOException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         DataListCollection rows = dataList.getRows();
         HashMap<String, StringBuilder> sb = getLabelAndKey(dataList);
         StringBuilder keySB = sb.get("key");
@@ -165,16 +168,15 @@ public class DownloadCsvOrExcelDatalistAction extends DataListActionDefault {
         String[] res = keySB.toString().split(",", 0);
         duplicates.setMap(findDuplicate(res));
 
-        if(includeCustomHeader()){
-            outputStream.write((getPropertyString("headerDecorator")+"\n").getBytes());
+        if (includeCustomHeader()) {
+            writer.write((getPropertyString("headerDecorator") + "\n"));
         }
-        outputStream.write((headerSB +"\n").getBytes());
+        writer.write((headerSB + "\n"));
 
-            
-        if(rowKeys != null && rowKeys.length > 0){
+        if (rowKeys != null && rowKeys.length > 0) {
 
             //goes through all the datalist row
-            for (int x=0; x<rows.size(); x++) {
+            for (int x = 0; x < rows.size(); x++) {
                 //compare with all the rowkeys that have been selected
                 for (String rowKey : rowKeys) {
                     //check instance of HashMap if not it will be Formrow
@@ -189,31 +191,51 @@ public class DownloadCsvOrExcelDatalistAction extends DataListActionDefault {
                     Object row = getRow(rows, x);
 
                     //get the keys and save it
-                    printCSV(dataList, outputStream, res, row);
+                    writeCSVContents(dataList, null, res, row, writer);
                 }
             }
 
-        }else if(getProperty("downloadAllWhenNoneSelected").equals("true")){
-
+        } else if (getProperty("downloadAllWhenNoneSelected").equals("true")) {
             //retrieve all rows
-            rows = dataList.getRows(0,0);
-
+            rows = dataList.getRows(0, 0);
             //goes through all the datalist row
-            for (int x=0; x<rows.size(); x++) {
-                Object row = getRow(rows,x);
+            for (int x = 0; x < rows.size(); x++) {
+                Object row = getRow(rows, x);
                 //get the keys and save it
-                printCSV(dataList, outputStream, res, row);
+                writeCSVContents(dataList, null, res, row, writer);
             }
         }
-        
-        if(getFooter()) {
-            outputStream.write((headerSB +"\n").getBytes());
+        if (getFooter()) {
+            writer.write((headerSB + "\n"));
         }
-        if(includeCustomFooter()){
-            outputStream.write((getPropertyString("footerDecorator")+"\n").getBytes());
+        if (includeCustomFooter()) {
+            writer.write((getPropertyString("footerDecorator") + "\n"));
+        }
+    }
+
+    private void writeCSVContents(DataList dataList, ByteArrayOutputStream outputStream, String[] res, Object row, PrintWriter writer) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String myStr : res) {
+            String value = getBinderFormattedValue(dataList, row, myStr);
+            String formattedValue = getBinderFormattedValue(dataList, row, myStr);
+            stringBuilder.append(formattedValue);
+            stringBuilder.append(",");
         }
 
-        return outputStream.toByteArray();
+        if (stringBuilder.length() > 0) {
+            stringBuilder.setLength(stringBuilder.length() - 1);
+        }
+        String value = stringBuilder.toString();
+        int chunkSize = value.getBytes().length;
+        writer.write("\r\n");
+        writer.write(value);
+        writer.flush();
+    }
+
+    protected void downloadExcel(HttpServletRequest request, HttpServletResponse response, DataList dataList, String[] rowKeys) throws ServletException, IOException {
+        Workbook workbook = getExcel(dataList, rowKeys);
+        String filename = getPropertyString("renameFile").equalsIgnoreCase("true") ? getPropertyString("filename") + ".xlsx" : "report.xlsx";
+        writeResponseExcel(request, response, workbook, filename, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet\n");
     }
 
     private HashMap<String, Integer> findDuplicate(String[] keySB) {
@@ -250,62 +272,62 @@ public class DownloadCsvOrExcelDatalistAction extends DataListActionDefault {
         String[] header = headerSB.toString().split(",", 0);
         duplicates.setMap(findDuplicate(res));
 
-        if(includeCustomHeader()){
+        if (includeCustomHeader()) {
             Cell titleCell = headerRow.createCell(0);
             String headerString = getPropertyString("headerDecorator");
             titleCell.setCellValue(headerString);
             int getNewLine = headerString.split("\r\n|\r|\n").length;
             headerRow.setHeightInPoints((getNewLine * sheet.getDefaultRowHeightInPoints()));
             sheet.autoSizeColumn(2);
-            sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, header.length-1));
-            rowCounter+=1;
+            sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, header.length - 1));
+            rowCounter += 1;
         }
 
         Row headerColumnRow = sheet.createRow(rowCounter);
         counter = 0;
-        for(String myStr: header) {
+        for (String myStr : header) {
             Cell headerCell = headerColumnRow.createCell(counter);
             headerCell.setCellValue(myStr);
             counter += 1;
         }
 
-        rowCounter +=1;
+        rowCounter += 1;
         counter = 0;
 
-        if(rowKeys != null && rowKeys.length > 0){
-            
-            //goes through all the datalist row
-            for (int x=0; x<rows.size(); x++) {
-                //compare with all the rowkeys that have been selected
-                for (int y=0; y<rowKeys.length; y++) {
-                    boolean boolInstance = rows.get(x) instanceof HashMap;
-                    boolean foundRowKey = foundRowKey(boolInstance,rows,x,rowKeys[y]);
+        if (rowKeys != null && rowKeys.length > 0) {
 
-                    if(!foundRowKey) {
+            //goes through all the datalist row
+            for (int x = 0; x < rows.size(); x++) {
+                //compare with all the rowkeys that have been selected
+                for (int y = 0; y < rowKeys.length; y++) {
+                    boolean boolInstance = rows.get(x) instanceof HashMap;
+                    boolean foundRowKey = foundRowKey(boolInstance, rows, x, rowKeys[y]);
+
+                    if (!foundRowKey) {
                         continue;
                     }
 
-                    printExcel(sheet,rowCounter,counter,rows,x,res,dataList);
+                    printExcel(sheet, rowCounter, counter, rows, x, res, dataList);
                     counter += 1;
-                    rowCounter+=1;
+                    rowCounter += 1;
 
                 }
             }
-            
-        }else if(getProperty("downloadAllWhenNoneSelected").equals("true")){
-            
+
+        } else if (getProperty("downloadAllWhenNoneSelected").equals("true")) {
+
             //retrieve all rows
-            rows = dataList.getRows(0,0);
+            rows = dataList.getRows(0, 0);
 
             //goes through all the datalist row
-            for (int x=0; x<rows.size(); x++) {
-                printExcel(sheet,rowCounter,counter,rows,x,res,dataList);
+            for (int x = 0; x < rows.size(); x++) {
+                printExcel(sheet, rowCounter, counter, rows, x, res, dataList);
                 counter += 1;
-                rowCounter+=1;
+                rowCounter += 1;
             }
         }
-        
-        if(getFooter()) {
+
+        if (getFooter()) {
             int z = 0;
             Row dataRow = sheet.createRow(rowCounter);
             for (String myStr : header) {
@@ -313,55 +335,28 @@ public class DownloadCsvOrExcelDatalistAction extends DataListActionDefault {
                 footerCell.setCellValue(myStr);
                 z += 1;
             }
-            rowCounter+=1;
+            rowCounter += 1;
         }
 
-        if(includeCustomFooter()){
+        if (includeCustomFooter()) {
             Row footerColumnRow = sheet.createRow(rowCounter);
             Cell titleCell = footerColumnRow.createCell(0);
             titleCell.setCellValue(getPropertyString("footerDecorator"));
-            sheet.addMergedRegion(new CellRangeAddress(rowCounter, rowCounter, 0, header.length-1));
+            sheet.addMergedRegion(new CellRangeAddress(rowCounter, rowCounter, 0, header.length - 1));
         }
 
         return workbook;
 
     }
 
-    /**
-     * Write to response for download
-     * @param request
-     * @param response
-     * @param bytes
-     * @param filename
-     * @param contentType
-     * @throws IOException
-     */
-    protected void writeResponse(HttpServletRequest request, HttpServletResponse response, byte[] bytes, String filename, String contentType) throws IOException, ServletException {
-        OutputStream out = response.getOutputStream();
-        try {
-            String name = URLEncoder.encode(filename, "UTF8").replaceAll("\\+", "%20");
-            response.setHeader("Content-Disposition", "attachment; filename="+name+"; filename*=UTF-8''" + name);
-            response.setContentType(contentType+"; charset=UTF-8");
-
-            if (bytes.length > 0) {
-                response.setContentLength(bytes.length);
-                out.write(bytes);
-            }
-        } finally {
-            out.flush();
-            out.close();
-            request.getRequestDispatcher(filename).forward(request, response);
-        }
-    }
-
     protected void writeResponseExcel(HttpServletRequest request, HttpServletResponse response, Workbook workbook, String filename, String contentType) throws IOException, ServletException {
         OutputStream out = response.getOutputStream();
         try {
             String name = URLEncoder.encode(filename, "UTF8").replaceAll("\\+", "%20");
-            response.setHeader("Content-Disposition", "attachment; filename="+name+"; filename*=UTF-8''" + name);
-            response.setContentType(contentType+"; charset=UTF-8");
+            response.setHeader("Content-Disposition", "attachment; filename=" + name + "; filename*=UTF-8''" + name);
+            response.setContentType(contentType + "; charset=UTF-8");
 
-            ByteArrayOutputStream ms=new ByteArrayOutputStream();
+            ByteArrayOutputStream ms = new ByteArrayOutputStream();
             workbook.write(ms);
 
             byte bytes[] = ms.toByteArray();
@@ -377,24 +372,24 @@ public class DownloadCsvOrExcelDatalistAction extends DataListActionDefault {
         }
     }
 
-    protected String getBinderFormattedValue(DataList dataList, Object o, String name){
+    protected String getBinderFormattedValue(DataList dataList, Object o, String name) {
         DataListColumn[] columns = dataList.getColumns();
         int skip = duplicates.getSkipCount(name);
         for (DataListColumn c : columns) {
-            if(c.getName().equalsIgnoreCase(name)){
+            if (c.getName().equalsIgnoreCase(name)) {
 
-                if(duplicates.checkKey(name)) {
-                    if(duplicates.skipCountLessThenDuplicate(name)) {
+                if (duplicates.checkKey(name)) {
+                    if (duplicates.skipCountLessThenDuplicate(name)) {
                         duplicates.addSkipCount(name);
                     }
-                    if(skip != 0) {
+                    if (skip != 0) {
                         skip -= 1;
                         continue;
                     }
                 }
 
                 String value;
-                try{
+                try {
                     value = DataListService.evaluateColumnValueFromRow(o, name).toString();
                     Collection<DataListColumnFormat> formats = c.getFormats();
                     if (formats != null) {
@@ -403,14 +398,14 @@ public class DownloadCsvOrExcelDatalistAction extends DataListActionDefault {
                                 value = f.format(dataList, c, o, value);
                                 String stripHTML = value.replaceAll("<[^>]*>", "");
                                 return stripHTML;
-                            }else{
+                            } else {
                                 return value;
                             }
                         }
-                    }else{
+                    } else {
                         return value;
                     }
-                }catch(Exception ex){
+                } catch (Exception ex) {
 
                 }
             }
@@ -438,31 +433,16 @@ public class DownloadCsvOrExcelDatalistAction extends DataListActionDefault {
         return sb;
     }
 
-
-    private void printCSV(DataList dataList, ByteArrayOutputStream outputStream, String[] res, Object row) throws IOException {
-        for(String myStr: res) {
-            String value = getBinderFormattedValue(dataList,row,myStr);
-
-            outputStream.write(value.trim().getBytes());
-            outputStream.write(",".getBytes());
-        }
-        String outputString = new String(outputStream.toByteArray());
-        outputString = outputString.substring(0, outputString.length() - 1);
-        outputStream.reset();
-        outputStream.write(outputString.getBytes());
-        outputStream.write("\n".getBytes());
-    }
-
     private void printExcel(Sheet sheet, int rowCounter, int counter, DataListCollection rows, int x, String[] res, DataList dataList) {
         Row dataRow = sheet.createRow(rowCounter);
-        Object row = getRow(rows,x);
+        Object row = getRow(rows, x);
         int z = 0;
-        for(String myStr: res) {
-            String value = getBinderFormattedValue(dataList,row,myStr);
+        for (String myStr : res) {
+            String value = getBinderFormattedValue(dataList, row, myStr);
             Cell dataRowCell = dataRow.createCell(z);
-            if( NumberUtils.isParsable(value) ){
+            if (NumberUtils.isParsable(value)) {
                 dataRowCell.setCellValue(Double.parseDouble(value));
-            }else{
+            } else {
                 dataRowCell.setCellValue(value);
             }
             z += 1;
@@ -474,7 +454,7 @@ public class DownloadCsvOrExcelDatalistAction extends DataListActionDefault {
     }
 
     private boolean foundRowKey(boolean boolInstance, DataListCollection rows, int x, String rowKey) {
-        if(boolInstance) {
+        if (boolInstance) {
             return ((HashMap) rows.get(x)).get("id").equals(rowKey);
         } else {
             return ((FormRow) rows.get(x)).get("id").equals(rowKey);
